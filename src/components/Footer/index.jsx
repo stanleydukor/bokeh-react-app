1b37fd7412232ef464a93ff4e9233f64e9c9c5f6
Stanley Dukor <stanleydukor@gmail.com>
2022-03-26 02:45:09 -0700

    modify footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { Box, Button, Flex, Text, IconButton, useColorMode } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import FooterStyle from "./style";
 
 export default function Footer() {
-  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <FooterStyle>
-      <Flex h="80px" px="30px" bg="transparent" alignItems="center" justifyContent="center">
-        <Text size="xs">CMPT 769 Project 2022</Text>
+      <Flex h="60px" px="30px" bg="transparent" alignItems="center" justifyContent="center">
+        <Text size="xxs">CMPT 769 Project 2022</Text>
       </Flex>
     </FooterStyle>
   );
