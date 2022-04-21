a88c338333a371e3598f07c7195479be47308e7f
Stanley Dukor <stanleydukor@gmail.com>
2022-04-21 13:53:01 -0700

    retain uploaded image preview

--- src/components/PictureInput/index.jsx
+++ src/components/PictureInput/index.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { imageActions } from "store/image";
+import { selectors as imageSelectors } from "store/image";
 import { Box, Flex, Text, IconButton, CloseButton } from "@chakra-ui/react";
 import { useColorModeValue } from "@chakra-ui/color-mode";
 import { AddIcon } from "@chakra-ui/icons";
 import * as Colors from "theme/colors";
 import PictureInputStyle from "./style";
+import { canvasToImage } from "utils/canvasUtils";
 
-const PictureInput = ({ id, text, labelProps, inputProps, handleChange }) => {
+const PictureInput = ({ id, text, labelProps, inputProps, handleChange, mainRgbCanvas, mainDepthCanvas }) => {
   const [file, setFile] = useState(null);
   const [preview, setPreview] = useState(null);
   const onHandleChange = e => {
@@ -24,6 +26,14 @@ const PictureInput = ({ id, text, labelProps, inputProps, handleChange }) => {
     setPreview(objectUrl);
     return () => URL.revokeObjectURL(objectUrl);
   }, [file]);
+  useEffect(() => {
+    if (mainRgbCanvas && id === "rgbImageUrl") {
+      setPreview(canvasToImage(mainRgbCanvas));
+    }
+    if (mainDepthCanvas && id === "depthImageUrl") {
+      setPreview(canvasToImage(mainDepthCanvas));
+    }
+  }, []);
   return (
     <PictureInputStyle>
       <Flex
@@ -37,19 +47,20 @@ const PictureInput = ({ id, text, labelProps, inputProps, handleChange }) => {
         justifyContent="center"
       >
         <Box
-          bg={file ? "none" : useColorModeValue(Colors.white[300], Colors.black[200])}
+          bg={preview ? "none" : useColorModeValue(Colors.white[300], Colors.black[200])}
           h="110px"
           w="110px"
           className="profile-image"
         >
           <label htmlFor={id} {...labelProps}>
-            {file ? <img src={preview} /> : <AddIcon h="30px" w="30px" />}
+            {preview ? <img src={preview} /> : <AddIcon h="30px" w="30px" />}
           </label>
-          {file && (
+          {preview && (
             <CloseButton
               className="close-button"
               onClick={() => {
                 setFile(null);
+                setPreview(null);
               }}
             />
           )}
@@ -71,8 +82,13 @@ const PictureInput = ({ id, text, labelProps, inputProps, handleChange }) => {
   );
 };
 
+const mapStateToProps = state => ({
+  mainRgbCanvas: imageSelectors.mainRgbCanvas(state),
+  mainDepthCanvas: imageSelectors.mainDepthCanvas(state)
+});
+
 const mapDispatchToProps = {
   handleChange: imageActions.handleChange
 };
 
-export default connect(null, mapDispatchToProps)(PictureInput);
+export default connect(mapStateToProps, mapDispatchToProps)(PictureInput);
